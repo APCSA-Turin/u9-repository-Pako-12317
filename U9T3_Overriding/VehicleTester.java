550f646515fd5eb1950665ef05a50f1d308b59cf
Pako-12317 <patrykm10@nycstudents.net>
2025-03-21 12:21:37 +0000

    U9T3 Final with Freestyle

--- U9T3_Overriding/VehicleTester.java
+++ U9T3_Overriding/VehicleTester.java
@@ -1,5 +1,12 @@
 public class VehicleTester {
     public static void main(String[] args) {
-        
+        Vehicle vehicle = new Vehicle("ABC123", 2.50, 3);
+        vehicle.isTaxi();
+        Car car = new Car("XYZ789", 3.00, 4, true);
+        car.isTaxi();
+        Taxi taxi = new Taxi("TAXI001", 4.00, 2, false, 0.0);
+        taxi.isTaxi();
+        Truck truck = new Truck("TRUCK001", 5.00, 1, 6, true);
+        truck.isTaxi();
     }
 }
